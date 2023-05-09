2780e3717297a3634e197a37d25e8e3707476f85
mariusrp <marius.e.phillips@gmai.com>
2023-05-09 14:33:23 +0200

    added home header

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -6,7 +6,7 @@ export default function Head() {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta  name="description" content="Google Clone created by Next js 13"/>
-    <title>Google Clone Next js</title>
+    <title>Google Clone Next js 13</title>
 </head>
   )
 }
