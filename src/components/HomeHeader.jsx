b9810cbc9e41c60b2d295c615a3f5c6631f78cce
mariusrp <marius.e.phillips@gmai.com>
2023-05-09 13:44:16 +0200

    Added home header

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -17,7 +17,7 @@ export default function HomeHeader() {
                 Images
             </Link>
                 <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2"/>
-                <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">Sign in</button>
+                <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">Sign In</button>
         </div>
     </header>
   )
