019a335e1514bbf5461d74d5b813f11388f163bc
mariusrp <marius.e.phillips@gmai.com>
2023-05-09 12:36:22 +0200

    install next js and create the first template

---
+++ src/app/head.jsx
@@ -0,0 +1,12 @@
+
+export default function Head() {
+  return (
+    <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta  name="description" content="Google Clone created by Next js 13"/>
+    <title>Google Clone Next </title>
+</head>
+  )
+}
