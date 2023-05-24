f0d2f9f6044f9f9c97949ccb66549a7a8fc39bcc
mariusrp <marius.e.phillips@gmai.com>
2023-05-24 10:31:40 +0200

    added image results

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,3 +1,28 @@
-export default function SearchImagePage() {
-  return <div>SearchImagePage</div>;
+import ImageSearchResults from '@/components/ImageSearchResults';
+import Link from 'next/link';
+
+export default async function SearchWebPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
+  if (!response.ok) {
+    throw new Error('Something went wrong');
+  }
+  const data = await response.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className='flex flex-col justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'>No results found</h1>
+        <p className='text-lg'>
+          Try searching something else or go back to the homepage{' '}
+          <Link href='/' className='text-blue-500'>
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return <>{results && <ImageSearchResults results={data} />}</>;
 }
